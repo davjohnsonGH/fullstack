8825b750e6229ae5e1fdcc4ec606681cacd9e6c2
davjohnsonGH <dezekielj@gmail.com>
2019-03-22 21:48:01 -0700

    Creates document 'thing'.

--- db/mongo/mongoose.js
+++ db/mongo/mongoose.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose');
 
 const route     = {};
-route.protocol  = 'mongodb://';
+route.protocol  = 'mongodb';
 route.host      = 'localhost';
-route.port      = ':27017/';
+route.port      = '27017';
 route.dbName    = 'anything';
 
 const connect = () => {
-   return mongoose.connect(route.protocol + route.host + route.port + route.dbName);
+   return mongoose.connect(route.protocol + '://' + route.host + ':' + route.port + '/' + route.dbName);
 }
 
 const thing = new mongoose.Schema({
     title: String,
-    id: Number,
     value: String,
     active: Boolean
 }) 
 
 const Thing = mongoose.model('thing', thing);
 
-console.log(Thing);
+connect()
+    .then(async connection => {
+        const thing = await Thing.create({
+            title: 'thing one',
+            value: 'thing-1',
+            active: true
+        })
+        console.log(thing);
+    })
+    .catch(e => console.error(e))
