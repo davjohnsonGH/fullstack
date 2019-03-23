5c173963b4e14440c23a6ab13d769f3aa3ec9fd0
davjohnsonGH <dezekielj@gmail.com>
2019-03-22 17:23:21 -0700

    Creates thing model with mongoose.

--- db/mongo/mongoose.js
+++ db/mongo/mongoose.js
@@ -1,10 +1,22 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const route     = {};
 route.protocol  = 'mongodb://';
 route.host      = 'localhost';
 route.port      = ':27017/';
 route.dbName    = 'anything';
+
 const connect = () => {
    return mongoose.connect(route.protocol + route.host + route.port + route.dbName);
 }
+
+const thing = new mongoose.Schema({
+    title: String,
+    id: Number,
+    value: String,
+    active: Boolean
+}) 
+
+const Thing = mongoose.model('thing', thing);
+
+console.log(Thing);
