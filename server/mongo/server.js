d6f15b7c5da875361e26573510407066bbc6506f
davjohnsonGH <dezekielj@gmail.com>
2019-03-24 17:27:11 -0700

    Captures mongo in client. Lazy use of npm CORS package.

--- server/mongo/server.js
+++ server/mongo/server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const { urlencoded, json } = require('body-parser');
 const mongoose = require('mongoose');
 const Thing = require('../../db/mongo/Thing');
+const cors = require('cors');
 
 const route     = {};
 route.protocol  = 'mongodb';
@@ -15,6 +16,7 @@ route.dbName    = 'anything';
 app.use(morgan('dev'));
 app.use(urlencoded( {extended: true} ));
 app.use(json());
+app.use(cors());
 
 // get all things
 app.get('/thing', async (req, res) => {
@@ -23,7 +25,7 @@ app.get('/thing', async (req, res) => {
     .lean()
     .exec();
     res.status(200).json(things);
-    res.send(things);
+    // res.send(things);
 })
 // create a thing
 app.post('/thing', async (req, res) => {
